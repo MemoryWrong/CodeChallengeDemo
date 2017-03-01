29af8cf8d3c340a8a56cb177984844abebeb81cd
MemoryWrong <287143005@qq.com>
2017-03-01 13:32:04 +0800

    first push demo project to GIT

---
+++ src/home.js
@@ -0,0 +1,18 @@
+/**
+ * Created by carlkane1987 on 28/2/17.
+ */
+import React from "react";
+import List from './components/list/list';
+import './index.css';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div>
+                <List />
+            </div>
+        )
+    }
+}
+
+export default Home;
